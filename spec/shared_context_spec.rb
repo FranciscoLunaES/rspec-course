177a21a84f54012ffd709c777c6a4c1c7ca08bc4
FranciscoLunaES <francisco.luna@enroutesystems.com>
2022-09-19 16:29:20 -0700

    Add no to method example and a test answers

--- spec/shared_context_spec.rb
+++ spec/shared_context_spec.rb
@@ -35,3 +35,34 @@ RSpec.describe 'second example group' do
     expect(some_variable).to eq([1, 2, 3])
   end
 end
+
+
+### ACTIVITY
+# 1. Subject will be equal to an empty array or object
+# 2. When you can modify the values in the subject
+#   subject(:luis) do
+#     {name: "luis"}
+#   end
+# 3. You have to put it instead of the name of the class and it helps
+# you if you need to change the name of the class in the future
+# you will can do it easier
+# 4. subject do
+#   [1,2]
+#  end
+
+# RSpec.describe 'second example group' do
+#   subject do
+#     [1, 2]
+#   end
+
+#   it 'subject array' do
+#     expect(subject).to eq([1, 2])
+#   end
+
+#   it { is_expected.to eq([1, 2]) }
+# end
+
+# I prefer the one liner
+
+# 5. One is to get all the shared examples and the other to get all the
+# Context or example group
