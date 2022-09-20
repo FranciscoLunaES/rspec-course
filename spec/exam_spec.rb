7172752d3f9ed7a356457ed9825feda6c425de8a
FranciscoLunaES <francisco.luna@enroutesystems.com>
2022-09-19 18:03:38 -0700

    Add all and be matcher examples and a knowledge test

---
+++ spec/exam_spec.rb
@@ -0,0 +1,30 @@
+=begin
+  1. expect({}).to be_empty
+  2. expect([10, 20, 30]).to all(be_even)
+  3.expect([0, 1, 3]).to all(be >= 0)
+  4. 
+  Truty
+  "hello"
+
+  0
+
+  -10
+
+  []
+
+  99.99
+
+  :hello
+
+  true
+
+  [1, 2, 3]
+
+  falsy
+
+  nil
+   
+  false
+
+
+=end
